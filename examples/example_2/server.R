2774826407cdb3fe77927985e1673c5cacada305
Chris Beeley <chris.beeley@gmail.com>
2019-10-27 12:57:41 +0000

    Minor updates to exercices
    Hopefully final commit? Taking a break for today

--- examples/example_2/server.R
+++ examples/example_2/server.R
@@ -10,7 +10,7 @@ function(input, output) {
   
   # Summary 
   
-  # shiny function here
+  # shiny text function here
     
     subset_data <- mapData %>%
       filter(year >= input$year[1], year <= input$year[2])
@@ -20,38 +20,36 @@ function(input, output) {
            length(unique(subset_data$year)), " occasions.")
   # })
   
-  # trend
-  
-  # shiny function here
-    
-    subset_data <- mapData %>%
-      filter(year >= input$year[1], year <= input$year[2])
-
-    thePlot = subset_data %>%
-      group_by(continent, year) %>%
-      summarise(meanLife = mean(lifeExp)) %>%
-      ggplot(aes(x = year, y = meanLife, group = continent, colour = continent)) +
-      geom_line() + ggtitle("Graph to show life expectancy by continent over time")
-    
-    if(input$linear){ 
-      thePlot = thePlot + geom_smooth(method = "lm") 
-    } 
-    
-    return(thePlot)
-  # })
-  
-  # draw map
-  
-  # shiny function here
-    
-    mapData %>%
-      filter(year == input$year[2]) %>%
-      leaflet() %>%
-      addTiles() %>%
-      setView(lng = 0, lat = 0, zoom = 2) %>%
-      addCircles(lng = ~ lon, lat = ~ lat, weight = 1,
-                 radius = ~ lifeExp * 5000, 
-                 popup = ~ paste(country, lifeExp))
-  # })
+  # # trend
+  # 
+  # # shiny graph function here
+  # 
+  #   subset_data <- mapData %>%
+  #     filter(year >= input$year[1], year <= input$year[2])
+  # 
+  #   thePlot = subset_data %>%
+  #     group_by(continent, year) %>%
+  #     summarise(meanLife = mean(lifeExp)) %>%
+  #     ggplot(aes(x = year, y = meanLife, group = continent, colour = continent)) +
+  #     geom_line() + ggtitle("Graph to show life expectancy by continent over time")
+  # 
+  #   # add test for checkbox here
+  # 
+  #   return(thePlot)
+  # # })
+  # 
+  # # draw map
+  # 
+  # # shiny function here
+  # 
+  #   mapData %>%
+  #     filter(year == input$year[2]) %>%
+  #     leaflet() %>%
+  #     addTiles() %>%
+  #     setView(lng = 0, lat = 0, zoom = 2) %>%
+  #     addCircles(lng = ~ lon, lat = ~ lat, weight = 1,
+  #                radius = ~ lifeExp * 5000,
+  #                popup = ~ paste(country, lifeExp))
+  # # })
   
 }
